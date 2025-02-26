31bbbd2edd9534f2f9f202a301e958c5cee04776
Sannbao <eag.03@mail.ru>
2025-02-26 18:42:22 +0300

    Calculator.java
    update

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,8 @@
-package lessons;
-
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Converter converter = new Converter();
         String[] actions = {"+", "-", "/", "*"};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
         Scanner scn = new Scanner(System.in);
@@ -19,51 +16,28 @@ public class Calculator {
                 break;
             }
         }
-
         if(actionIndex==-1){
             System.out.println("Некорректное выражение");
             return;
         }
-
-
         String[] data = exp.split(regexActions[actionIndex]);
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
-            int a,b;
-            boolean isRoman = converter.isRoman(data[0]);
-            if(isRoman){
-
-                a = converter.romanToInt(data[0]);
-                b = converter.romanToInt(data[1]);
-
-            }else{
-                a = Integer.parseInt(data[0]);
-                b = Integer.parseInt(data[1]);
-            }
-            int result;
-            switch (actions[actionIndex]){
-                case "+":
-                    result = a+b;
-                    break;
-                case "-":
-                    result = a-b;
-                    break;
-                case "*":
-                    result = a*b;
-                    break;
-                default:
-                    result = a/b;
-                    break;
-            }
-            if(isRoman){
-                System.out.println(converter.intToRoman(result));
-            }
-            else{
-                System.out.println(result);
-            }
-        }else{
-            System.out.println("Числа должны быть в одном формате");
-        }
-
-
+        int a,b;
+        a = Integer.parseInt(data[0]);
+        b = Integer.parseInt(data[1]);
+        int result;
+        switch (actions[actionIndex]){
+            case "+":
+                result = a+b;
+                break;
+            case "-":
+                result = a-b;
+                break;
+            case "*":
+                result = a*b;
+                break;
+            default:
+                result = a/b;
+                break;
+        }System.out.println(result);
     }
 }
